9d99a75d1b3464081fc0962755c425fa163917d0
Gustavo Carvalho Hendges <209403@upf.br>
2026-05-14 16:39:59 -0300

    first test

---
+++ main.py
@@ -0,0 +1,26 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = genai.Client(
+    api_key =os.getenv("GEMINI_API_KEY")
+)
+
+chat = client.chats.create(
+    model="gemini-2.5-flash"
+)
+
+print("Assistente iniciado! Digite 'sair' para encerrar.\n")
+
+while True:
+    mensagem = input("Você: ")
+
+    if mensagem.lower() == "sair":
+        break
+
+    resposta = chat.send_message(mensagem)
+
+    print("\nIA:", resposta.text)
+    print()
