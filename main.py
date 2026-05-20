84a34e58ae7b8e7dca92b1f622d6819513ab7b17
Gustavo Carvalho <gch1412@proton.me>
2026-05-19 21:35:24 -0300

    Arquitetura modificada

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-from google import genai
-from dotenv import load_dotenv
 from fastapi import FastAPI
-from pydantic import BaseModel
-import os
-
-load_dotenv()
+from routes.chat import router as chat_router
 
 app = FastAPI()
-
-client = genai.Client(
-    api_key = os.getenv("GEMINI_API_KEY")
-)
-
-chat = client.chats.create(
-    model="gemini-2.5-flash"
-)
-
-class Message(BaseModel):
-    prompt: str
 
 @app.get("/")
 def root():
     return {"message": "A API está funcionando"}
 
-@app.post("/chat")
-def conversar(message: Message):
-
-    resposta = chat.send_message(message.prompt)
-
-    return { 
-        "response": resposta.text
-    }
+app.include_router(chat_router, prefix="/chat")
---
+++ routes/chat.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from services.geminiService import ask_gemini
+
+router = APIRouter()
+
+class Message(BaseModel):
+    prompt: str
+
+@router.post("/")
+def chat(message: Message):
+    response = ask_gemini(message.prompt)
+
+    return {
+        "response": response
+    }
