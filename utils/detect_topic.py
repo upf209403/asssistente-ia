09b58f8b626339da1b783894fc23af3c0f8d5946
Gustavo Carvalho <gch1412@proton.me>
2026-05-28 20:32:06 -0300

    detect_tpoc & load_knowledge atualizados

--- utils/detect_topic.py
+++ utils/detect_topic.py
@@ -1,3 +1,31 @@
 def detect_topic(prompt: str):
-    if(["loops"] in prompt):
-        pass
+    prompt = prompt.lower()
+
+    topics = []
+
+    if any(word in prompt for word in ["condicional", "condicionais", " if", "elif", "else"]): topics.append("condicionais.md")
+
+    if any(word in prompt for word in ["função", "funcao", "funções", "funcoes", "argumento", "argumentos"]): topics.append("funcoes.md")
+
+    if any(word in prompt for word in ["variável", "variavel", "variáveis", "variaveis", "constante", "constantes"]): topics.append("variaveis.md")
+
+    if any(word in prompt for word in ["repetição", "repetiçao", "repeticao", "laços de repetição", "laços de repetiçao", " for ", "while"]): topics.append("loops.md")
+
+    if not topics:
+        return """
+            Não foi possível identificar o tópico da conversa.
+
+            Esse assistente é restrito a:
+
+            - Pensamento computacional
+            - Variaveis
+            - Tipos de dados
+            - Operadores
+            - Estruturas condicionais
+            - Estruturas de repetição
+            - Funções
+
+            Reformule sua busca utilizando os conceitos acima.
+        """
+
+    return topics
--- utils/load_knowledge.py
+++ utils/load_knowledge.py
@@ -1,2 +1,18 @@
-def load_knowledge(topic: str):
-    pass
+from pathlib import Path
+
+KNOWLEDGE_PATH = "../knowledge"
+
+def load_knowledge(files):
+
+    contents = []
+
+    for file in files:
+        path = Path(KNOWLEDGE_PATH)/file
+
+        if path.exists():
+            with open(path, "r", encoding="utf-8") as f:
+
+                contents.append(
+                    f.read()
+                )
+    return "\n\n".join(contents)
