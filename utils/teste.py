c0b7a4ed8a2e7c4b27753cf3ea8db6b2c8fe07f8
Gustavo Carvalho <gch1412@proton.me>
2026-06-18 20:01:39 -0300

    loops.md atualizado

--- utils/teste.py
+++ utils/teste.py
@@ -1,13 +1,20 @@
 numero = 8
 
-
-
 def define_numero():
     global numero 
     numero = 5
     return numero
     
-define_numero()
-print(numero)
-print(define_numero())
+rodando = True
 
+while rodando:
+    nome = input("Digite um nome: ")
+
+    if(nome):
+        print(nome)
+
+        if(nome.lower()[0] == "z"):
+            rodando = False
+            print("Programa finalizado")
+    else:
+        print("String vazia")
